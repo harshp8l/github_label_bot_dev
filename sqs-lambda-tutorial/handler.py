13efe30b9d0ada67d5745202750079b64ce62bfe
Harsh Patel <harshpatel081296@gmail.com>
2018-10-01 12:17:17 -0700

    Initial serverless python file config

--- sqs-lambda-tutorial/handler.py
+++ sqs-lambda-tutorial/handler.py
@@ -1,24 +1,26 @@
-import json
+import os
+import boto3
 
+SQS_CLIENT = boto3.client('sqs')
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
+def start(event, context):
     """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
+    First Lambda Function
+    :param event: AWS event data
+    :param context: AWS function's event
     """
+    print(SQS_CLIENT.send_message(
+        QueueUrl=os.getenv('SQS_URL'),
+        MessageBody = 'test'
+    ))
+    return ''
+
+def end(event, context):
+    """
+    Second Lambda Function
+    :param event: AWS event data(from SQS)
+    :param event: AWS function's context
+    """
+    print(event)
+    return ''
+
