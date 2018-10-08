49f962a9d42434b64d6e18bed6d3e679ba4863b0
Harsh Patel <harshpatel081296@gmail.com>
2018-10-08 12:32:07 -0700

    Updated with parsing json (not needed)

--- sqs-lambda-tutorial/handler.py
+++ sqs-lambda-tutorial/handler.py
@@ -1,5 +1,7 @@
 import os
 import boto3
+import json
+from pprint import pprint
 
 SQS_CLIENT = boto3.client('sqs')
 
@@ -9,14 +11,21 @@ def start(event, context):
     :param event: AWS event data
     :param context: AWS function's event
     """
+    
+    # Grabbing only necessary event information to send
+    # data = json.loads(str(event))['repository']['html_url']
+
     print(SQS_CLIENT.send_message(
         QueueUrl=os.getenv('SQS_URL'),
         MessageBody = str(event)
     ))
+
+
+    # Successful response -- assuming message will be sent correctly
     return {
         "statusCode": 200,
         "headers": { "Content-Type": "application/json"},
-        "body":'hi' 
+        "body":'Success' 
     }
 
 def end(event, context):
@@ -25,5 +34,6 @@ def end(event, context):
     :param event: AWS event data(from SQS)
     :param event: AWS function's context
     """
+
     print(event)
     return ''
